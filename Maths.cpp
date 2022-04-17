b5dddac110b313511ffeacc9ab5944e736d3cab0
Charmy-cyanide <Charmy-cyanide@github.com>
2022-04-17 17:17:36 +1000

    Rename var n to c

--- Maths.cpp
+++ Maths.cpp
@@ -5,7 +5,7 @@ void error() {
 }
 int main() {
 	setlocale(LC_ALL, "rus");
-	int a = 0;
+	int c = 0;
 	float m1[3][3], m2[3][3], res[3][3];
 
 	//Информационное сообщение
@@ -14,16 +14,16 @@ int main() {
 	std::cout << "3. Умножение матрицы на число" << std::endl;
 	std::cout << "Введите номер операции, которую необходимо провести с матрицами:";
 	//Ввод с клавиатуры
-	std::cin >> a;
+	std::cin >> c;
 	//Защита от неверного ввода
-	while (a != 1 && a != 2 && a != 3) {
+	while (c != 1 && c != 2 && c != 3) {
 		std::cout << "Это не номер операции, пожалуйста выберите одну из трёх оперций:" << std::endl;
 		std::cout << "1. Сложение матриц" << std::endl;
 		std::cout << "2. Вычитание матриц" << std::endl;
 		std::cout << "3. Умножение матрицы на число" << std::endl;
-		std::cin >> a;
+		std::cin >> c;
 	}
-	if (a == 1) {
+	if (c == 1) {
 		std::cout << "Введите матрицы для их суммирования" << std::endl;
 		std::cout << "Первая матрица: " << std::endl;
 		for (int i = 0; i < 3; i++) {
@@ -35,7 +35,7 @@ int main() {
 		}
 		plus(m1, m2);
 	}
-	else if (a == 2) {
+	else if (c == 2) {
 		std::cout << "Введите матрицы для вычисления их разности" << std::endl;
 		std::cout << "Первая матрица из которой вычитают: " << std::endl;
 		for (int i = 0; i < 3; i++) {
@@ -47,7 +47,7 @@ int main() {
 		}
 		minus(m1, m2);
 	}
-	else if (a == 3) {
+	else if (c == 3) {
 		float a;
 		std::cout << "Введите матрицу для умножения на число: " << std::endl;
 		for (int i = 0; i < 3; i++) {
