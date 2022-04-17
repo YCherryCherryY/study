e4a87bb23a1e6d9ff8a5df69ee4d9fb72cc48c98
Charmy-cyanide <Charmy-cyanide@github.com>
2022-04-17 16:19:47 +1000

    Some comments too

--- functions.cpp
+++ functions.cpp
@@ -1,7 +1,7 @@
 #include "functions.h"
 #include <iostream>
 
-void plus(float arr1[3][3], float arr2[3][3]) { //Сложение
+void plus(float arr1[3][3], float arr2[3][3]) { //Сложение матриц
 	setlocale(LC_ALL, "rus");
 	float arr[3][3];
 	for (int i = 0; i < 3; i++)
